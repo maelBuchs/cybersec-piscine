be9f7dad51f66842b1db23a317564c6e0a99d1b6
Mael <mael@buchs.fr>
2024-09-22 02:29:38 +0200

    almost done

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -2,6 +2,8 @@ import requests
 import sys
 import os
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import argparse
 
 filesFound = 0
 filesDownloaded = 0
@@ -11,6 +13,7 @@ class color:
 	GREEN = '\033[92m'
 	YELLOW = '\033[93m'
 	DEFAULT = '\033[0m'
+
 
 def getIndex(link):
     pos = -1
@@ -30,29 +33,33 @@ def downloadImages(link, imgs):
 	mainDir = link.lstrip('htps:/.')
 	for img in imgs:
 		if img.startswith('https://') or img.startswith('http://'):
-			os.makedirs(mainDir + '/' + img[:img.rfind('/')], exist_ok = True)
+			os.makedirs('data/' + mainDir  + '/' + img[:img.rfind('/')], exist_ok = True)
 			imgfile = requests.get(img, timeout=10, verify=True)
 		else:
-			os.makedirs(mainDir + '/' + img[:img.rfind('/')], exist_ok = True)
-			imgfile = requests.get(link + '/' + img, timeout=10, verify=True)
+			os.makedirs('data/' + mainDir + '/' + img[:img.rfind('/')], exist_ok = True)
+			imgfile = requests.get(urljoin(link, img), timeout=10, verify=True)
 		if not imgfile.ok:
 			if img.startswith('https://') or img.startswith('http://'):
 				print(color.RED + "Error: couldn't download " + img + color.DEFAULT)
 			else:
-				print(color.RED + "Error: couldn't download " + link + '/' + img + color.DEFAULT)
+				print(color.RED + "Error: couldn't download " + urljoin(link, img) + color.DEFAULT)
 		else:
 			filesDownloaded += 1
 			if img.startswith('https://') or img.startswith('http://'):
 				print(color.GREEN + "Succes : " + color.DEFAULT + img + " was downloaded")
 			else:
 				print(color.GREEN + "Succes : " + color.DEFAULT + link + img + " was downloaded")
-			with open(mainDir + '/' + img, 'wb') as f:
+			with open('./data/' + mainDir + '/' + img, 'wb') as f:
 				f.write(imgfile.content)
 				
 
 def catchImages(link):
 	global filesFound
-	site = requests.get(link, timeout=10, verify=True)
+	try:
+		site = requests.get(link, timeout=10, verify=True)
+	except requests.exceptions.RequestException as e:
+		print(color.RED + "Error: couldn't reach " + link + color.DEFAULT)
+		return []
 	soup = BeautifulSoup(site.content, features="html.parser")
 	if not site.ok:
 		print("Error: target returned the " + str(site.status_code) + " error code.")
@@ -64,7 +71,7 @@ def catchImages(link):
 		if i.get('href').startswith('http') or i.get('href').startswith('https'):
 			recursLinks.append(i.get('href').split('#')[0])
 		else:
-			recursLinks.append(link + '/' + i.get('href').split('#')[0])
+			recursLinks.append(urljoin(link, i.get('href').split('#')[0]))
 	print(str(len(recursLinks)) + " links found")
 	imgsLinks = []
 	for img in imgs:
@@ -80,12 +87,39 @@ def catchImages(link):
 
 
 def main():
-	if len(sys.argv) != 2:
+	recursions = 0
+	argparser = argparse.ArgumentParser(description='Download images from a website')
+	argparser.add_argument('-r', action='store_true', help='Recursively download images from the links found on the website')
+	argparser.add_argument('-l', type=int, help='Select the depth of the recursion')
+	argparser.add_argument('-p', action='store_true', help='Change the path of the download')
+	argparser.add_argument('target', type=str, help='The target website')
+		# print("Error: missing target")
+		# exit()
+	args = argparser.parse_args()
+	if not args.target:
 		print("Error: missing target")
 		exit()
+	if args.r:
+		recursions = 5
+	if args.l:
+		if not args.r:
+			print("ausage: spider.py [-h] [-r] [-l] [-p] [depth] target")
+			exit()
+		if args.depth:
+			recursions = args.depth
+		else:
+			print("Error: missing depth")
+			exit()
+	if args.p:
+		if args.path:
+			os.chdir(args.path)
+		else:
+			print("Error: missing path")
+			exit()
 	list = [[]]
-	list[0]+= catchImages(sys.argv[1])
-	recursions = 2
+	if not args.target.startswith('http'):
+		args.target = 'http://' + args.target
+	list[0]+= catchImages(args.target)
 	for i in range(recursions):
 		list.append([])
 		for j in list[i]:
